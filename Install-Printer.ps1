f408aaf7703941581f568b1f51c0ae8565bf9b49
Gregory Zuckerman <gregory.zuckerman@gmail.com>
2023-08-31 10:36:50 +0100

    Update Install-Printer.ps1
    Fixed issue where the PowerShell command was incorrect and removed the IP address of the printer used to test.

--- Install-Printer.ps1
+++ Install-Printer.ps1
@@ -2,6 +2,7 @@
 
 .Synopsis
 Created on:   17/08/2023
+Updated on:   31/08/2023
 Created by:   Gregory Zuckerman
 Filename:     Install-Printer.ps1
 
@@ -10,17 +11,15 @@ Simple PowerShell Script to install a printer driver and printer via Microsoft I
 ### Powershell Commands ###
 
 Install:
-
-powershell.exe ExecutionPolicy Bypass .\Install-Printer.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\Install-Printer.ps1
 
 Remove:
-
-powershell.exe ExecutionPolicy Bypass .\Remove-Printer.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\Remove-Printer.ps1
 
 Detection:
 
 Rule Type:          Registry
-Key path:           HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Print\Printers\Sharp MXC304WFH
+Key path:           HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Print\Printers\Sharp_MXC304WFH
 Value:              Name
 Detection method:   String comparison
 Operator:           Equals
@@ -33,19 +32,18 @@ Name:               Sharp MXC304WFH
 
 $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 $drivername = "SHARP UD3 PCL6"
-$portName = "IP_10.11.8.156"
-$PortAddress = "10.11.8.156"
+
+# Set the IP address to match the IP of the printer you are installing
+$portName = "IP_XX.XX.XX.XX"
+$PortAddress = "XX.XX.XX.XX"
 
 # Staging Drivers
-
 pnputil.exe /add-driver "$psscriptroot\sv0ejenu.inf" /install
 
 # Installing Drivers
-
 Add-PrinterDriver -Name $drivername
 
 # Install Printerport | Check if the port already exist
-
 $checkPortExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
 if (-not $checkPortExists) 
 {
@@ -53,14 +51,12 @@ Add-PrinterPort -name $portName -PrinterHostAddress $PortAddress
 }
 
 # Check if Printer Driver Exists
-
 $printDriverExists = Get-PrinterDriver -name $DriverName -ErrorAction SilentlyContinue
 
 # Install Printer
-
 if ($printDriverExists)
 {
-Add-Printer -Name "Sharp MXC304WFH" -PortName $portName -DriverName $DriverName
+Add-Printer -Name "Sharp_MXC304WFH" -PortName $portName -DriverName $DriverName
 }
 else
 {
